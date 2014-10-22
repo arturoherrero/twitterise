beb1034486ecaae8d3069b2eddc2f540df819384
Arturo Herrero <arturo.herrero@gmail.com>
2014-10-23 00:44:44 +0100

    Twitter client with logger

--- lib/twitter_client.rb
+++ lib/twitter_client.rb
@@ -1,7 +1,9 @@
+require "logger"
 require "twitter"
 
 class TwitterClient
   def initialize
+    @logger = Logger.new(STDOUT)
     @client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
       config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
@@ -13,24 +15,28 @@ class TwitterClient
   # Returns an array of numeric IDs
   # If not user_id is specified, refers to the implicit authenticated user.
   def following(user_id = nil)
+    logger.info "Get following of #{user_id || 'me'}"
     client.friend_ids(user_id).to_a
   end
 
   # Returns an array of numeric IDs
   # If not user_id is specified, refers to the implicit authenticated user.
   def followers(user_id = nil)
+    logger.info "Get followers of #{user_id || 'me'}"
     client.follower_ids(user_id).to_a
   end
 
   def follow(user_id)
+    logger.info "Follow #{user_id}"
     client.follow(user_id)
   end
 
   def unfollow(user_id)
+    logger.info "Unfollow #{user_id}"
     client.unfollow(user_id)
   end
 
   private
 
-  attr_reader :client
+  attr_reader :logger, :client
 end
